74541115ca1f24b83f853b663b9a8db5ab1018aa
AlevtinAntonov <111906191+AlevtinAntonov@users.noreply.github.com>
2023-06-19 15:04:28 +0300

    Update homeworks/task_2.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- homeworks/task_2.py
+++ homeworks/task_2.py
@@ -15,7 +15,7 @@ result = None
 
 if a + b < c or b + c < a or c + a < b:
     result = 'Треугольник с такими сторонами не существует'
-elif a == b and b == c and a == c:
+elif a == b == c:
     result = 'Треугольник равносторонний'
 elif a == b or b == c or a == c:
     result = 'Треугольник равнобедренный'
