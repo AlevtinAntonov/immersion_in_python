ae76c7a2b30583d810604a36ffcc7a4d1f5e7b30
Alevtin <antonov3017@gmail.com>
2023-06-20 12:14:39 +0300

    added task_2.py

--- homework_2/task_2.py
+++ homework_2/task_2.py
@@ -2,80 +2,53 @@
 # Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions.
 from fractions import Fraction
 
-print(Fraction(5, 16) + Fraction(7, 12))
-# def calculate_lcd(x, y):
-#     if x > y:
-#         greater = x
-#     else:
-#         greater = y
-#     while True:
-#         if greater % x == 0 and greater % y == 0:
-#             lowest_common_d = greater
-#             break
-#         greater += 1
-#     return lowest_common_d
-#
-# def add_multiplier(x, y, lcd):
-#     add_multiplier_1 = lcd / x
-#     add_multiplier_2 = lcd / y
-#     return add_multiplier_1, add_multiplier_2
-#
-# def greater_common_divider(x, y):
-#     while y:
-#         x, y = y, x % y
-#     return x
-#
-input_fractions = input('Введите две дроби через пробел в формате - a/b c/d ').split()
+input_fractions = input('Введите две дроби через пробел и знак + или * в формате - a/b c/d + ').split()
 
-fraction_1, fraction_2 = map(str, input_fractions)
+fraction_1, fraction_2, signs = map(str, input_fractions)
 a, b = map(int, str(fraction_1).split('/'))
 c, d = map(int, str(fraction_2).split('/'))
-# lowest_common_denominator = calculate_lcd(b, d)
-#
-#
-# add_multiplier_1, add_multiplier_2 = add_multiplier(b, d, lowest_common_denominator)
-# e = int(a * add_multiplier_1 + c * add_multiplier_2)
-#
-#
-# print(f'Cумма дробей {a}/{b} и {c}/{d} = {e}/{lowest_common_denominator}')
 
-def lcm(a, b):
-    """Нахождение наименьшего общего кратного двух чисел"""
+print('Fractions сумма: ', Fraction(a, b) + Fraction(c, d))
+print('Fractions произведение: ', Fraction(a, b) * Fraction(c, d))
+
+
+def lowest_common_multiple(a, b):
     if a == 0 or b == 0:
         return 0
     else:
-        return abs(a * b) // gcd(a, b)
-def gcd(a, b):
-    """Нахождение наибольшего общего делителя двух чисел"""
+        return abs(a * b) // greatest_common_divisor(a, b)
+
+
+def greatest_common_divisor(a, b):
     while b:
         a, b = b, a % b
     return a
 
 
-def add_fractions(a, b, c, d):
-    """Сложение двух дробей"""
-    # Находим общий знаменатель
-    common_denominator = lcm(b, d)
-
-    # Приводим первую дробь к общему знаменателю
-    a *= common_denominator // b
-    b = common_denominator
-
-    # Приводим вторую дробь к общему знаменателю
-    c *= common_denominator // d
-    d = common_denominator
-
-    # Складываем дроби
-    numerator = a + c
-
-    # Сокращаем дробь
-    divisor = gcd(numerator, common_denominator)
+def fraction_reduction(numerator, common_denominator):
+    divisor = greatest_common_divisor(numerator, common_denominator)
     numerator //= divisor
     common_denominator //= divisor
 
     return numerator, common_denominator
 
-l, m = add_fractions(a,b,c,d)
-print(l, m)
+
+def calc_fractions(a, b, c, d):
+    match signs:
+        case '+':
+            common_denominator = lowest_common_multiple(b, d)
+            a *= common_denominator // b
+            c *= common_denominator // d
+            numerator = a + c
+            numerator, common_denominator = fraction_reduction(numerator, common_denominator)
+            # numerator, denominator = addition_fractions(a, b, c, d)
+        case '*':
+            numerator = a * c
+            common_denominator = b * d
+            numerator, common_denominator = fraction_reduction(numerator, common_denominator)
+    return numerator, common_denominator
 
 
+numerator, denominator = calc_fractions(a, b, c, d)
+
+print(f'Результат {a}/{b} {signs} {c}/{d} = {numerator}/{denominator}')
