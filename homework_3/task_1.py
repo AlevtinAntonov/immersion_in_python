405cc5eec9a38f2dc977cc94adf806e696cb2cec
Alevtin <antonov3017@gmail.com>
2023-06-22 16:57:07 +0300

    task_4.py

--- homework_3/task_1.py
+++ homework_3/task_1.py
@@ -6,46 +6,117 @@
 # ✔ Какие вещи есть у всех друзей кроме одного и имя того, у кого данная вещь отсутствует
 # ✔ Для решения используйте операции с множествами. Код должен расширяться на любое большее количество друзей.
 
-tourists = {
-    'Alex': ('Палатка', 'Спальник', 'Фонарик', 'Миска', 'Хлеб', 'Карта'),
-    'Oleg': ('Спальник','Спички', 'Миска', 'Кружка', 'Консервы', 'Компас'),
-    'Dima': ('Спальник','Топор', 'Миска', 'Кружка', 'Крупа', 'Телефон', 'Батарейки')
+
+friends = {
+    'Alex': ('Спальник', 'Миска', 'Спички', 'Фонарик', 'Карта', 'Палатка'),
+    'Oleg': ('Спальник', 'Миска', 'Кружка', 'Спички', 'Консервы', 'Компас'),
+    'Dima': ('Спальник', 'Миска', 'Кружка', 'Топор', 'Крупа', 'Телефон')
 }
-one = list(tourists.values())
-print(one)
-def list_atribute_frends(dict_list):
-    list_atribute = []
-    for atribut in dict_list.values():
-        list_atribute += atribut
-    return list_atribute
+count = len(friends)
+name_friends = []
+for friend in friends:
+    name_friends.append(friend)
+
+def all_things(dict_name):
+    common_set = set()
+    for k, v in dict_name.items():
+        common_set = common_set.union(set(v))
+    return common_set
+
+def unique_things(dict_name):
+    unique_set = set()
+    count = len(dict_name)
+    other_things = set()
+    friend_name = 'Alex'
+    for name_friend in name_friends:
+        for k, v in dict_name.items():
+            if k != name_friend:
+                other_things = other_things.union(set(v))
+            unique_one_set = all_things(dict_name).difference(other_things)
+            unique_set = unique_set.union(unique_one_set)
+
+    return unique_set
 
 
-# Какие вещи уникальны, есть только у одного друга
-# и имя того, у кого данная вещь отсутствует
-count = 0
-# for i in list_atribute_frends(friends):
-#     for j in list_atribute_frends(friends):
-#         if j == i:
-#             count += 1
+    #
+    # for k, v in dict_name.items():
+    #     unique_set = set(dict_name[k]).difference()
 
-def dict_unicum_atribute (dict_list):
-    name_not_atribute = []
-    new_dict = {}
-    count = 0
-    for key, value in tourists.items():
-        name_not_atribute.append(key)
-        new_dict[key] = []
-        for j in value:
-            for k in list_atribute_frends(tourists):
-                if k == j:
-                    count += 1
-            if count == 1:
-                new_dict[key] += [j]
-            if count > 1:
-                name_not_atribute.remove(key)
-            count = 0
-    print("у этого списка людей отсутствует вещь, которая дублируется у остальных", name_not_atribute)
-    return new_dict
+    # pass
 
-print(f"Вещи которые взяли три друга{sorted(set(list_atribute_frends(tourists)))}")
-# print(f"Уникальные вещи которые взял каждый друг {dict_unicum_atribute(friends)}")
+def all_friends_except_one(dict_name):
+    pass
+
+unique_set = set(friends['Alex']).difference(set(friends['Oleg']) | (set(friends['Dima'])))
+
+
+    # difference_set = common_set.difference(set(v))
+    # intersection_set = intersection_set.intersection(set(v))
+
+print(f'Все три друга взяли: {all_things(friends)}')
+print(f'Уникальные вещи есть только у одного друга- {unique_things(friends)}')
+print(f'Вещи уникальны: {unique_set}')
+# print(f'ПЕресечение: {sorted(intersection_set)}')
+
+
+# my_set = {3, 4, 2, 5, 6, 1, 7}
+# other_set = {1, 4, 42, 314}
+# new_set = my_set.union(other_set)
+# print(f'{my_set = }\n{other_set = }\n{new_set = } union')
+# print()
+#
+my_set = {3, 4, 2, 5, 6, 1, 7}
+other_set = {1, 4, 42, 314}
+new_set = my_set.intersection(other_set)
+print(f'{my_set = }\n{other_set = }\n{new_set = } intersection ')
+print()
+#
+# my_set = {3, 4, 2, 5, 6, 1, 7}
+# other_set = {1, 4, 42, 314}
+# new_set = my_set.difference(other_set)
+# print(f'{my_set = }\n{other_set = }\n{new_set = } difference ')
+##########################
+
+# tourists = {
+#     'Alex': ('Палатка', 'Спальник', 'Фонарик', 'Миска', 'Хлеб', 'Карта'),
+#     'Oleg': ('Спальник','Спички', 'Миска', 'Кружка', 'Консервы', 'Компас'),
+#     'Dima': ('Спальник','Топор', 'Миска', 'Кружка', 'Крупа', 'Телефон', 'Батарейки')
+# }
+# one = list(tourists.values())
+# print(one)
+# def list_atribute_frends(dict_list):
+#     list_atribute = []
+#     for atribut in dict_list.values():
+#         list_atribute += atribut
+#     return list_atribute
+#
+#
+# # Какие вещи уникальны, есть только у одного друга
+# # и имя того, у кого данная вещь отсутствует
+# count = 0
+# # for i in list_atribute_frends(friends):
+# #     for j in list_atribute_frends(friends):
+# #         if j == i:
+# #             count += 1
+#
+# def dict_unicum_atribute (dict_list):
+#     name_not_atribute = []
+#     new_dict = {}
+#     count = 0
+#     for key, value in tourists.items():
+#         name_not_atribute.append(key)
+#         new_dict[key] = []
+#         for j in value:
+#             for k in list_atribute_frends(tourists):
+#                 if k == j:
+#                     count += 1
+#             if count == 1:
+#                 new_dict[key] += [j]
+#             if count > 1:
+#                 name_not_atribute.remove(key)
+#             count = 0
+#     print("у этого списка людей отсутствует вещь, которая дублируется у остальных", name_not_atribute)
+#     return new_dict
+#
+# print(f"Вещи которые взяли три друга{sorted(set(list_atribute_frends(tourists)))}")
+# # print(f"Уникальные вещи которые взял каждый друг {dict_unicum_atribute(friends)}")
--- homework_3/task_4.py
+++ homework_3/task_4.py
@@ -3,12 +3,45 @@
 # Достаточно вернуть один допустимый вариант.
 # *Верните все возможные варианты комплектации рюкзака.
 
-things = {'зажигалка': 20, 'компас': 100, 'фрукты': 500, 'рубашка': 300,
-        'термос': 1000, 'аптечка': 200, 'куртка': 600, 'бинокль': 400, 'удочка': 1200,
-          'салфетки': 40, 'бутерброды': 820, 'палатка': 5500, 'спальный мешок': 2250, 'жвачка': 10}
-ves = int(input()) * 1000
-sorted_things = dict(sorted(things.items(), key=lambda x: -x[1]))
-for k, v in sorted_things.items():
-    if v <=  ves:
-        print(k, sep='/n')
-        ves -= v
+BACKPACK_CAPACITY = 3_000
+MIN_CAPACITY = 2_000
+things = {'спальник': 2000, 'пенка': 200, 'спички': 20, 'футболка': 300, 'термос': 1500, 'аптечка': 200,
+          'бинокль': 400, 'удочка': 700, 'палатка': 5500, 'консервы': 2000, 'фонарик': 100, }
+
+sorted_things = dict(sorted(things.items(), key=lambda val: val[1]))
+weight = 0
+backpack_set = {}
+list_weight = []
+for k, v in things.items():
+    list_weight.append(int(v))
+    if weight <= BACKPACK_CAPACITY:
+        print(weight, int(v))
+        weight += int(v)
+        backpack_set[k] = v
+
+print(len(backpack_set),backpack_set, weight)
+
+from itertools import combinations
+def combine(dict, d):
+    return list(combinations(dict,d))
+
+dictA = { 1: 19, 2: 21, 3: 18, 4: 25, 5: 30,}
+set = 4
+print(combine(dictA, set))
+
+def subset_sum(numbers, target, partial=[]):
+    s = sum(partial)
+
+    # check if the partial sum is equals to target
+    if s <= target and s > MIN_CAPACITY:
+        print(f"Weight > {MIN_CAPACITY} and < {target} set = {partial} " )
+    if s > target:
+        return  # if we reach the number why bother to continue
+
+    for i in range(len(numbers)):
+        n = numbers[i]
+        remaining = numbers[i + 1:]
+        subset_sum(remaining, target, partial + [n])
+
+
+subset_sum(list_weight, BACKPACK_CAPACITY)
