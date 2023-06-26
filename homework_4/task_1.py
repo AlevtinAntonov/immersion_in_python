b72c334449cbf79fb3d7983eacdb469d52975a69
Alevtin <antonov3017@gmail.com>
2023-06-26 14:13:01 +0300

    task_1.py added

--- homework_4/task_1.py
+++ homework_4/task_1.py
@@ -3,3 +3,32 @@
 # на s (кроме переменной из одной буквы s) на None. Значения не удаляются, а помещаются
 # в одноимённые переменные без s на конце.
 
+def func():
+    vars_name_lst = ['datas', 's', 'names', 'sx']
+    for var_name in vars_name_lst:
+        if var_name.endswith('s') and var_name != 's':
+            globals()[f'{var_name[:-1:]}'] = globals()[f'{var_name}']
+            globals()[f'{var_name}'] = None
+
+
+datas = [42, -73, 12, 85, -15, 2]
+s = 'Hello World'
+names = 'NoName', 'OtherName', 'NewName'
+sx = 42
+
+print(f'{datas=}')
+print(f'{s=}')
+print(f'{names=}')
+print(f'{sx=}')
+
+func()
+
+print(f'{datas=}')
+print(f'{s=}')
+print(f'{names=}')
+print(f'{sx=}')
+
+print(f'{data=}')
+print(f'{s=}')
+print(f'{name=}')
+print(f'{sx=}')
