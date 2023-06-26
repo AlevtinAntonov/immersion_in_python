b4c4c1a266e0a47252cb57d54c6cd421ff7fed57
Alevtin <antonov3017@gmail.com>
2023-06-26 15:32:16 +0300

    task_2.py added

--- homework_4/task_2.py
+++ homework_4/task_2.py
@@ -1,1 +1,9 @@
 # Напишите функцию для транспонирования матрицы
+
+def matrix_transpose(lst):
+    return [list(i) for i in zip(*lst)]
+
+
+my_lst = [[1, 2, 3], [4, 5, 6]]
+print(my_lst)
+print(matrix_transpose(my_lst))
