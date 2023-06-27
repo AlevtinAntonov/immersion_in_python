d3c71b95db8e42141221d572146cd217573d2479
Alevtin <alevtin_antonov@mail.ru>
2023-06-27 22:09:57 +0300

    sem_5

--- seminars/s_5/task_5.py
+++ seminars/s_5/task_5.py
@@ -5,3 +5,23 @@
 # ✔ Если число кратно и 3, и 5, то программа должна выводить слово «FizzBuzz».
 # ✔ *Превратите решение в генераторное выражение.
 
+print(*('FizzBuzz' if num % 15 == 0 else 'Buzz' if num % 5 == 0 else 'Fizz' if num % 3 == 0 else num for num in
+        range(1, 101)))
+
+
+def gen():
+    for num in range(1, 101):
+        if num % 15 == 0:
+            yield 'FizzBuzz'
+        elif num % 5 == 0:
+            yield 'Buzz'
+        elif num % 3 == 0:
+            yield 'Fizz'
+        else:
+            yield num
+
+
+print(*gen(), sep=' ')
+
+gen_2 = ('Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or i for i in range(1, 101))
+print(*gen_2)
