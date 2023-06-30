9ceced0ef9c6e873373d050a48837ee31be73d3f
Alevtin <alevtin_antonov@mail.ru>
2023-06-30 19:31:02 +0300

    logger.py.py

--- homework_4/logger.py
+++ homework_4/logger.py
@@ -4,7 +4,6 @@ from datetime import datetime as dt
 def LOG(func):
     def wrapper(*args):
         result = func(*args)
-        print(*args)
         with open('log.csv', 'a', encoding='utf-8') as log:
             log.write(
                 f'Функция: {func.__name__}; {func.__doc__}; Баланс/Операция № {args}; {dt.now()}\n')
