59cc5b75c7361a6394035ca909e5a22948dfa053
Alevtin_linux_home <antonov3017@gmail.com>
2023-07-02 15:37:12 +0300

    4 5 6

---
+++ seminars/s_6/task_4_run.py
@@ -0,0 +1,3 @@
+from  task_4 import zg
+
+zg('Text zg', {'One', 'one', 'ONE'}, 5)
--- seminars/s_6/task_6.py
+++ seminars/s_6/task_6.py
@@ -6,5 +6,7 @@
 # � Отдельно напишите функцию, которая выводит результаты угадывания из защищённого словаря в удобном для чтения виде.
 # � Для формирования результатов используйте генераторное выражение.
 
-print('Statistics:\n' + '\n'.join(f'FOR:  {k:<50}' + f'SUCCESS FROM TRY №: {v}' * (v != 0) +
-       'ALL TRIES UNSUCCESSFUL' * (v == 0) for k, v in my_dict.items()))
+# print('Statistics:\n' + '\n'.join(f'FOR:  {k:<50}' + f'SUCCESS FROM TRY №: {v}' * (v != 0) +
+#        'ALL TRIES UNSUCCESSFUL' * (v == 0) for k, v in my_dict.items()))
+
+_global_dict = {}
