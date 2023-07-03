624a22acb9f97f64db93569ee88dbd2ebbada260
Alevtin <antonov3017@gmail.com>
2023-07-03 11:38:02 +0300

    task 7

--- seminars/s_6/task_7.py
+++ seminars/s_6/task_7.py
@@ -4,3 +4,22 @@
 # � Для простоты договоримся, что год может быть в диапазоне [1, 9999].
 # � Весь период (1 января 1 года - 31 декабря 9999 года) действует Григорианский календарь.
 # � Проверку года на високосность вынести в отдельную защищённую функцию.
+# Задание №8
+# � Создайте пакет с всеми модулями, которые вы создали за время занятия.
+# � Добавьте в __init__ пакета имена модулей внутри дандер __all__.
+# � В модулях создайте дандер __all__ и укажите только те функции, которые могут верно работать за пределами модуля.
+def _check_leap_year(year):
+    return not year % 4 != 0 or year % 100 == 0 and year % 400 != 0
+
+def check_date(date):
+    day, month, year = map(int, date.split('.'))
+    if not 0 < day < 32 or not 0 < month < 13 or not 0 < year < 10000:
+        return False
+    if month in [4, 6, 9, 11] and day > 30:
+        return False
+    leap = _check_leap_year(year)
+    if leap and month == 2 and day > 29:
+        return False
+    if not leap and month == 2 and day > 28:
+        return False
+    return True
