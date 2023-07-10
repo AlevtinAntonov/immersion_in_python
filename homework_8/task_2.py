7ab910b7cbad70d6a01693b9fbfef07a25b8f4b0
Alevtin <alevtin_antonov@mail.ru>
2023-07-10 22:11:19 +0300

    hw_8 task_2.py

--- homework_8/task_2.py
+++ homework_8/task_2.py
@@ -1,2 +1,19 @@
 # Прочитайте созданный в прошлом задании csv файл без использования csv.DictReader.
 # Распечатайте его как pickle строку.
+
+import csv
+import pickle
+
+
+def read_csv_file(file):
+    """
+    read csv file without csv.DictReader
+    :param file: input csv file
+    :return:
+    """
+    with open(file, 'r',  encoding='utf-8') as f:
+        file_reader = csv.reader(f)
+        print(pickle.dumps(list(file_reader)))
+
+if __name__ == '__main__':
+    read_csv_file('task_1.csv')
