cfff970fa852601fae5905dcfef497e814734299
Alevtin <antonov3017@gmail.com>
2023-07-13 11:10:11 +0300

    hw_9 solution

---
+++ homework_9/pack_seminar_9/task_2.py
@@ -0,0 +1,38 @@
+# Задание №2
+# Дорабатываем задачу 1.
+# Превратите внешнюю функцию в декоратор.
+# Он должен проверять входят ли переданные в функциюугадайку числа в диапазоны [1, 100] и [1, 10].
+# Если не входят, вызывать функцию со случайными числами из диапазонов.
+
+from typing import Callable
+from random import randint
+from functools import wraps
+
+
+def deco_guess_number(func):
+    @wraps(func)
+    def wrapper(num: int, count: int):
+        if not 1 <= num <= 100:
+            num = randint(1, 100)
+        if not 1 <= count <= 10:
+            count = randint(1, 10)
+        result = func(num, count)
+        return result
+
+    return wrapper
+
+@deco_guess_number
+def game(num: int, count: int):
+    for i in range(1, count + 1):
+        print(f"Попытка номер {i} ")
+        user_num = int(input("Введите число от 1 до 100: "))
+        if user_num == num:
+            return "Вы угадали!"
+        elif user_num < num:
+            print("Ваше число меньше")
+        else:
+            print("Ваше число больше")
+
+
+if __name__ == '__main__':
+    print(game(50, 5))
--- homework_9/quadratic_equation.py
+++ homework_9/quadratic_equation.py
@@ -11,18 +11,18 @@ from functools import wraps
 from pathlib import Path
 from typing import Callable
 
+
 def data_from_csv_wrap(file_name: str):
     def deco(func: Callable):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            lst_solution = []
             with open(file_name, 'r', encoding='utf-8') as csv_file:
-                reader = csv.reader(csv_file)
-                for i, row in enumerate(reader):
-                    if i == 0:
-                        continue
-                    args = (int(j) for j in row)
-                    result = func(*args, **kwargs)
-                    yield result
+                reader = csv.reader(csv_file, quoting=csv.QUOTE_NONNUMERIC)
+                for row in reader:
+                    x_1, x_2 = func(row[0], row[1], row[2])
+                    lst_solution.append([row[0], row[1], row[2], x_1, x_2])
+            return lst_solution
 
         return wrapper
 
@@ -31,50 +31,30 @@ def data_from_csv_wrap(file_name: str):
 
 def save_results_to_json(func):
     file = Path(f"{func.__name__}.json")
-    if file.is_file():
-        with open(file, 'r', encoding='utf-8') as f:
-            json_file = json.load(f)
-    else:
-        json_file = []
 
     def wrapper(*args, **kwargs):
-        for result in func(*args, **kwargs):
-            if result:
-                dct = {'args': args,
-                       'kwargs': kwargs,
-                       'result': str(result)}
-                json_file.append(dct)
-                with open(file, 'w', encoding='utf-8') as json_f:
-                    json.dump(json_file, json_f, indent=2)
-            else:
-                break
+        lst_solution = [func(*args, **kwargs)]
+        json_res = [dict(a=v[0], b=v[1], c=v[2], x_1=v[3], x_2=v[4]) for v in lst_solution[0]]
+        with open(file, 'w', encoding='utf-8') as json_f:
+            json.dump(json_res, json_f, indent=2)
+        return lst_solution
 
     return wrapper
+
+
 @save_results_to_json
 @data_from_csv_wrap('random.csv')
 def solving_quadratic_equation(a: int, b: int, c: int):
-    """
-        Решает квадратное уравнение ax^2 + bx + c = 0
-
-        :param a: коэффициент при x^2
-        :param b: коэффициент при x
-        :param c: свободный член
-        :return: корни уравнения
-        """
     discriminant = b ** 2 - 4 * a * c
-    print(discriminant)
-    if a == 0:
-        x = -c / b
-        return x
-    elif discriminant < 0:
-        return 'Нет решения'
+    if discriminant < 0:
+        x_1 = x_2 = 'None'
+    elif a == 0:
+        x_1, x_2 = -c / b, 'None'
     elif discriminant == 0:
-        x = -b / 2 * a
-        return x
+        x_1, x_2 = -b / 2 * a, 'None'
     else:
-        x_1 = (-b + discriminant ** .5) / (2 * a)
-        x_2 = (-b - discriminant ** .5) / (2 * a)
-        return x_1, x_2
+        x_1, x_2 = (-b + discriminant ** .5) / (2 * a), (-b - discriminant ** .5) / (2 * a)
+    return x_1, x_2
 
 
 def generating_csv_file(file_name: str = 'random.csv'):
@@ -85,10 +65,9 @@ def generating_csv_file(file_name: str = 'random.csv'):
     rows = []
     for _ in range(number_of_rows):
         a, b, c = random.sample(range(min_number, max_number), 3)
-        rows.append({'a': a, 'b': b, 'c': c})
+        rows.append([a, b, c])
     with open(file_name, 'w', newline='', encoding='utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames=['a', 'b', 'c'])
-        writer.writeheader()
+        writer = csv.writer(f)
         writer.writerows(rows)
 
 
