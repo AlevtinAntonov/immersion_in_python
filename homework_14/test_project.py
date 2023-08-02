1a6b0b5110f1abf7472a5af320b8fc6577700bf3
Alevtin <antonov3017@gmail.com>
2023-08-02 09:52:10 +0300

    hw_14 test_project.py

--- homework_14/test_project.py
+++ homework_14/test_project.py
@@ -1,46 +1,53 @@
 import pytest
-import json
 from user import User
 from project import Project
 
 
-# @pytest.fixture
-# def get_file(tmp_path):
-#     f_name = tmp_path / 'test_file.json'
-#     with open(f_name, 'w+', encoding='utf-8') as f:
-#         print(f'{f=}')
-#         yield f
-
-
 @pytest.fixture
-def get_file(tmp_path):
-    f_name = tmp_path / 'test_file.txt'
-    print(f'Создаю файл {f_name}')  # принтим в учебных целях
-    with open(f_name, 'w+', encoding='utf-8') as f:
-        yield f
-    print(f'Закрываю файл {f_name}')  # принтим в учебных целях
-
-def set_data(get_file):
-    print(f'Заполняю файл {get_file.name} цифрами') # принтим в
-    for i in range(10):
-        get_file.write('User(name="Иван", user_id=123, level=1)')
-    get_file.seek(0)
-
-# @pytest.fixture
-# def set_data(get_file):
-#     temp = [Project(User(name='Иван', user_id=123, level=1))]
-#     return temp
+def create_users():
+    user1 = User('Иван', 123, 1)
+    user2 = User(name='Наталья', user_id=741, level=5)
+    user3 = User(name='Антон', user_id=456, level=2)
+    return [user1, user2, user3]
 
 
-def test_add_user(get_file, set_data):
-    pr_user = Project(set_data)
-    pr_user.enter('Иван', 123, 1)
-    assert pr_user.admin == pr_user
+def test_create_user():
+    user = User('Иван', 123, 1)
+    assert user.name == 'Иван'
+    assert user.user_id == 123
+    assert user.level == 1
 
 
-# def test_first_char(set_char):
-#     first = set_char.read(5)
-#     assert first == 'ABCD'  # специально провалим тест
+def test_enter(create_users):
+    user = create_users[0]
+    with Project(create_users) as p:
+        p.enter('Иван', 123, 1)
+        assert user.name == 'Иван'
+        assert user.user_id == 123
+        assert user.level == 1
+
+
+def test_add_user_not_entered(create_users):
+    with Project(create_users) as p:
+        p.add_user('Алевтин', 1001, 1)
+        assert 'Пользователь не добавлен! Для добавления пользователя войдите в систему.'
+
+
+def test_add_user_entered(create_users):
+    with Project(create_users) as p:
+        p.enter('Иван', 123, 1)
+        p.add_user('Алевтин', 1002, 2)
+        assert create_users[3].name == 'Алевтин'
+        assert create_users[3].user_id == 1002
+        assert create_users[3].level == 2
+
+
+def test_del_user(create_users):
+    with Project(create_users) as p:
+        p.enter('Иван', 123, 1)
+        p.enter('Антон', 456, 2)
+        p.del_user('Иван', 123, 1)
+        assert create_users[0].name != 'Иван'
 
 
 if __name__ == '__main__':
